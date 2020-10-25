37e61203382fd90094883fb8b3ec01a5c6dbdc1e
Debabrata Mukherjee <debtechno@gmail.com>
2020-10-25 22:59:09 +0530

    Updates for Student attendance module 25-10-2020

---
+++ src/app/studentAttendance.ts
@@ -0,0 +1,11 @@
+export class StudentAttendance{
+    student_id:number; 
+    first_name: String;
+    last_name: String;       
+    class_name: String;
+    roll_no: number;
+    section: String;
+    date: Date;
+    is_active: String;
+    is_present: String;
+}
